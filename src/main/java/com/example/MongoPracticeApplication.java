1a3b0f5c68d66ed4cf38fa638debbd6e3f7b76d5
sankar-kumar <sankarkumar2709@gmail.com>
2022-04-20 12:34:23 +0530

    Added changes for scheduler jobs

--- src/main/java/com/example/MongoPracticeApplication.java
+++ src/main/java/com/example/MongoPracticeApplication.java
@@ -5,14 +5,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication	
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages="com.example")
+@EnableScheduling
 public class MongoPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoPracticeApplication.class, args);
+	}
+
+//	@Scheduled(fixedDelay = 1000)
+	@Scheduled(cron = "* 2 * * * *")
+	public void scheduleFixedDelayTask() {
+		System.out.println("TimeSchedule triggered..");
+		System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
 	}
 
 }
--- src/main/java/com/example/adapter/PlayerDetailsAdapter.java
+++ src/main/java/com/example/adapter/PlayerDetailsAdapter.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.example.model.PlayerDetails;
 
+@Component
 public class PlayerDetailsAdapter {
 
 	public static Logger logger = LoggerFactory.getLogger(PlayerDetailsAdapter.class);
